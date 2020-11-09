d046cabcd28f48318f4bc8fdd9679d7bca5fa56f
Rick Janusz <rjanusz@conversantmedia.com>
2020-11-09 17:05:48 -0600

    added slicemasters pages

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,7 +1,78 @@
 import React from 'react';
+import { graphql, Link } from 'gatsby';
+import Img from 'gatsby-image'; 
+import styled from 'styled-components';
 
-export default function SlicemastersPage() {   
-    return <>
-        <p>Hey: I'm theee slice masters page</p>
-    </>
+
+const SlicemasterGrid = styled.div`
+display: grid;
+grid-gap: 2rem;
+grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+`;
+
+
+
+const SingleSlicemaster = styled.div`
+a {
+    text-decoration: none;
 }
+.gatsby-image-wrapper {
+    height: 400px;
+}
+h2 {
+    transform: rotate(-2deg);
+    text-align: center;
+    font-size: 4rem;
+    margin-bottom: -2rem;
+    position: relative;
+    z-index: 2;
+
+}
+`;
+
+export default function SlicemastersPage({ data }) { 
+    const slicemasters = data.slicemasters.nodes;
+    console.log(slicemasters); 
+    return (
+        <>
+            <SlicemasterGrid>
+                {slicemasters.map((person) => (
+                    <SingleSlicemaster>
+                    <Link to={`/slicemasters/${person.slug.current}`}>
+                        <h2>
+                            <span className="mark">
+                                {person.name}
+                            </span>
+                        </h2>
+                    </Link>
+
+                    <Img fluid={person.image.asset.fluid} />
+                    <p className="description">{person.description}</p>
+                    </SingleSlicemaster>
+                ))}
+            </SlicemasterGrid>
+        </>
+    );
+}
+
+export const query = graphql`
+    query {
+        slicemasters: allSanityPerson {
+          nodes {
+            id
+            name
+            slug {
+              current
+            }
+            description
+            image {
+              asset {
+                fluid(maxWidth: 410) {
+                  ...GatsbySanityImageFluid
+                }
+              }
+            }
+          }
+        }
+    }
+`;
